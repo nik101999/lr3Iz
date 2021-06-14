8253ecc072bbd5cada0f6dc6c20ac4faa296aa38
nik101999 <85872177+nik101999@users.noreply.github.com>
2021-06-14 23:58:29 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -77,28 +77,28 @@ def draw(filename,cho):
 
 
 ##меняем половинки
-factor = cho
-for i in range(width):
-	for j in range(height):
-		rand = random.randint(-factor, factor)
-		a = pix[i, j][0] + rand
-		b = pix[i, j][1] + rand
-		c = pix[i, j][2] + rand
-		if (a < 0):
-			a = 0
-		if (b < 0):
-			b = 0
-		if (c < 0):
-			c = 0
-		if (a > 255):
-			a = 255
-		if (b > 255):
-			b = 255
-		if (c > 255):
-			c = 255
-		draw.point((i, j), (a, b, c))
-  output_filename = filename
-  img.save(output_filename)
+ factor = cho
+ 	 for i in range(width):
+ 		 for j in range(height):
+ 			 rand = random.randint(-factor, factor)
+			 a = pix[i, j][0] + rand
+			 b = pix[i, j][1] + rand
+			 c = pix[i, j][2] + rand
+			 if (a < 0):
+				 a = 0
+			 if (b < 0):
+				 b = 0
+			 if (c < 0):
+				 c = 0
+			 if (a > 255):
+				 a = 255
+			 if (b > 255):
+				 b = 255
+			 if (c > 255):
+				 c = 255
+		 	 draw.point((i, j), (a, b, c))
+	   output_filename = filename
+	   img.save(output_filename)
  
  return output_filename,gr_path
 
