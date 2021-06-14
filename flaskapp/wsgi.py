a04af7aa6a1c6f472df53b720046471c1563868f
nik101999 <85872177+nik101999@users.noreply.github.com>
2021-06-15 02:47:08 +0700

    Update wsgi.py

--- flaskapp/wsgi.py
+++ flaskapp/wsgi.py
@@ -2,3 +2,12 @@
 from iz import app
 if __name__ == "__main__":
   app.run()
+
+app.get('/app/:id', doEverythingInOneHugeFunctionWithAsyncBranches);
+  
+app.get('/app/:id', checkUserAuth, findApp, renderView, sendJSON);
+
+function checkUserAuth(req, res, next) {
+ if (req.session.user) return next();
+ return next(new NotAuthorizedError());
+}
